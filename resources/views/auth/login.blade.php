a8fbf32c95317e12f2fd833151632e02bf81a2d0
Douglas Vicentini Ferreira <dvferreira@bartofil.com.br>
2024-02-28 14:54:40 -0300

    Removing unnecessary google login code

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -79,30 +79,4 @@
             </div>
         </div>
     </div>
-    <script>
-        function onSuccess(googleUser) {
-            console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-        }
-
-        function onFailure(error) {
-            console.log(error);
-        }
-
-        function renderButton() {
-            gapi.signin2.render('my-signin2', {
-                'scope'    : 'profile email',
-                'width'    : 240,
-                'height'   : 50,
-                'longtitle': true,
-                'theme'    : 'dark',
-                'onsuccess': onSuccess,
-                'onfailure': onFailure
-            });
-        }
-
-        gapi.load('auth2', function () {
-            gapi.auth2.init();
-        })
-    </script>
-    <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
 @endsection
